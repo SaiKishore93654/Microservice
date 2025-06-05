c6756d8db55c024537baf1b3ddb72fe61e1a79a7
SaiKishore93654 <151427498+SaiKishore93654@users.noreply.github.com>
2025-06-05 13:18:09 +0530

    Dockerfile

--- cart-service/index.js
+++ cart-service/index.js
@@ -1,6 +1,9 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 5002;
+
+app.use(cors());
 
 app.get('/cart', (req, res) => {
   res.json({ cart: ["Laptop", "Phone"] });
